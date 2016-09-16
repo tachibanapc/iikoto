0236cd9f8f35c3901331f9290d9458246c737444
Rose <lain@lain.org.uk>
2016-09-16 15:38:07 +0100

    Thread view.

--- iikoto.rb
+++ iikoto.rb
@@ -20,7 +20,7 @@ class Imageboard < Sinatra::Base
   # Enable Rack CSRF protection and flashes.
   enable :sessions
   register Sinatra::Flash
-  
+
   # This is the route for the homepage.
   get '/' do
     locals = {
@@ -48,6 +48,25 @@ class Imageboard < Sinatra::Base
   end
 
   # Thread view page.
-  # get '/:board/thread/:number' do
-  # end
+  get '/:board/thread/:number' do
+    board = Board.find_by(route: params[:board])
+    yarn = Yarn.find_by(number: params[:number])
+    
+    if board.nil?
+      flash[:error] = "The board you selected doesn't exist!"
+      redirect '/'
+    elsif yarn.nil?
+      flash[:error] = "The thread you specified doesn't exist!"
+      redirect "/#{board.route}"
+    else
+      locals = {
+        board: board,
+        boards: Board.all,
+        yarn: yarn,
+        replies: Post.where(yarn: yarn.number)[1..-1]
+      }
+      
+      erb :thread, :locals => locals
+    end
+  end
 end
