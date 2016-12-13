2c424eb94db56ea11776f29009b5e2685713f72d
Rose <lain@lain.org.uk>
2016-12-13 22:15:30 +0000

    Phil's from the soul, Jack's from the PI.

--- iikoto.rb
+++ iikoto.rb
@@ -9,14 +9,28 @@ require_relative 'core_ext'
 
 # Read the config file.
 begin
-  CONFIG = YAML.load_file('config.yml').freeze
-rescue Error::ENOENT
+  $CONFIG = YAML.load_file('config.yml').freeze
+
+  # An assertion function for fields in the config.
+  def assert_config(field)
+    if !$CONFIG.has_key?(field)
+      raise "The config file is missing #{field}" unless $CONFIG.has_key?(field)
+    end
+  end
+
+  assert_config(:connection)
+  assert_config(:max_filesize)
+  assert_config(:reply_limit)
+  assert_config(:image_limit)
+  assert_config(:default_name)
+rescue Exception => e
+  puts e
   puts "The config file 'config.yml' is missing."
-  exit
+  exit 1
 end
 
 # Establish ActiveRecord's base connection.
-ActiveRecord::Base.establish_connection(CONFIG[:connection])
+ActiveRecord::Base.establish_connection($CONFIG[:connection])
 
 class Imageboard < Sinatra::Base
   # Enable Rack CSRF protection and flashes.
--- routes/board.rb
+++ routes/board.rb
@@ -1,6 +1,6 @@
 class Imageboard
   # Board index page.
-  get('/:board') do
+  get '/:board' do
     redirect "/#{params[:board]}/"
   end
 
@@ -20,5 +20,35 @@ class Imageboard
 
       slim :board, locals: locals
     end
+  end
+
+  post '/:board/' do
+    board = Board.find_by(route: params[:board])
+
+    if board.nil?
+      flash[:error] = "The board you selected doesn't exist!"
+      redirect '/'
+    end
+
+    if !params.has_key? "file"
+      flash[:error] = "You can't start a thread with no file!"
+      redirect "/#{board.route}"
+    end
+
+    filetype = params[:file][:type]
+
+    if !filetype.match(/image\/(jp(e)?g|png|gif)/)
+      flash[:error] = "The file you provided is of invalid type."
+      redirect "/#{board.route}"
+    end
+
+    file = file[:file][:tempfile]
+
+    if file.size > $CONFIG[:max_filesize]
+      flash[:error] = "The file you provided is too large."
+      redirect "/#{board.route}"
+    end
+
+    "u did it fam"
   end
 end
