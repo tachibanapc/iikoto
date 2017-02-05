51ff9f1cf012c0907d88543bbd50d0b0209edd82
Rose <lain@lain.org.uk>
2017-02-05 19:47:18 +0000

    added image reply count to catalog page

--- models/yarn.rb
+++ models/yarn.rb
@@ -6,4 +6,18 @@ class Yarn < ActiveRecord::Base
   def replies
     Post.where(yarn: self.number)[1..-1]
   end
+
+  def images
+    self.replies.select { |post|
+      !post.file.nil?
+    }
+  end
+
+  def reply_limit?
+    self.replies.length > $CONFIG[:reply_limit]
+  end
+
+  def image_limit?
+    self.images.length > $CONFIG[:image_limit]
+  end
 end
