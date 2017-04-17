8f93fa132a2697fe92e60b7e27b8bbb7e6d2df09
zwwwdr <zwdr@cock.li>
2017-04-17 02:57:04 +0200

    Moved post-body transformations and added quoting

--- models/post.rb
+++ models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   def file
     Image.find_by(post: self.number) 
   end
+
+  def formatBody
+    self.body.sub(/\>\>(?<you>\d+)/, '<a href="#p\k<you>">>>\k<you></a>')
+      .gsub(/\>(?<greentext>.+)/, '<span class="quote">>\k<greentext></span>')
+  end
 end
